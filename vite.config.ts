4649a2d5c0beba7756b91df849bf50137bc9a22c
Kenya Masuko <ken12rug@gmail.com>
2024-09-30 22:52:16 +0900

    chore: vite ssg

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import honox from "honox/vite";
+import client from "honox/vite/client";
+import { defineConfig } from "vite";
+import ssg from "@hono/vite-ssg";
 
-export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+    };
+  }
+
+  const entry = "./app/server.ts";
+
+  return {
+    plugins: [ssg({ entry }), honox()],
+  };
+});
